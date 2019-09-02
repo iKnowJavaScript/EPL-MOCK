48d407980683a689ad19160bb7983ad85cab8c50
iKnowJavaScript <remymartins0398@yahoo.com>
2019-09-02 13:59:29 +0100

    Add some helper functions

---
+++ config/passport.js
@@ -0,0 +1,21 @@
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const { jwtSecret } = require('./env');
+// const User = require('../models/user.model');
+const { UserQuery } = require('../api/models');
+
+const jwtOptions = {
+  secretOrKey: jwtSecret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer')
+};
+
+const jwt = async (payload, done) => {
+  try {
+    const user = await UserQuery.findById(payload.sub);
+    if (user) return done(null, user);
+    return done(null, false);
+  } catch (error) {
+    return done(error, false);
+  }
+};
+
+exports.jwt = new Strategy(jwtOptions, jwt);
