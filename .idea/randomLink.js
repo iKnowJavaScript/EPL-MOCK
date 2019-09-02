904097644fe43fcb91126336431b2b76771d48a8
iKnowJavaScript <remymartins0398@yahoo.com>
2019-09-02 17:59:42 +0100

    add idea folder

---
+++ .idea/randomLink.js
@@ -0,0 +1,33 @@
+function getrandom() {
+  var random_string =
+    Math.random(5, 10)
+      .toString(32)
+      .substring(5, 15) +
+    Math.random(5, 10)
+      .toString(32)
+      .substring(5, 15);
+  return random_string;
+}
+
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
+console.log(getrandom());
