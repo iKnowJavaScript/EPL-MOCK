fc8f1b714b4d0755f0ad773c6a1baf28029b9984
iKnowJavaScript <remymartins0398@yahoo.com>
2019-09-03 03:55:57 +0100

    Add fixture routes to index router

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoutes = require('./user.route');
 const adminRoutes = require('./admin.route');
 const teamRoutes = require('./team.route');
+const fixtureRoutes = require('./fixture.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -18,5 +19,8 @@ router.use('/admin', adminRoutes);
 
 // mount team routes at /team
 router.use('/team', teamRoutes);
+
+// mount fixture routes at /fixture
+router.use('/fixture', fixtureRoutes);
 
 module.exports = router;
