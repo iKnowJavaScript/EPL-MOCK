9adc3a22290130b729fb5266e843204fe4ac8cac
iKnowJavaScript <remymartins0398@yahoo.com>
2019-09-03 03:39:59 +0100

    Add fixture validation

---
+++ api/validations/fixture.validation.js
@@ -0,0 +1,43 @@
+const { Joi } = require('celebrate');
+
+module.exports = {
+  create: {
+    body: {
+      date: Joi.date().required(),
+      time: Joi.string().required(),
+      home_team: Joi.string()
+        .hex()
+        .required(),
+      away_team: Joi.string()
+        .hex()
+        .required(),
+      stadium: Joi.string().required(),
+      status: Joi.string(),
+      result: Joi.string(),
+      isDeleted: Joi.boolean()
+    }
+  },
+  editTeam: {
+    body: {
+      date: Joi.date(),
+      time: Joi.string(),
+      home_team: Joi.string().hex(),
+      away_team: Joi.string().hex(),
+      stadium: Joi.string(),
+      status: Joi.string(),
+      result: Joi.string(),
+      isDeleted: Joi.boolean()
+    },
+    params: {
+      id: Joi.string().hex()
+    }
+  },
+  delete: {
+    body: {
+      isDeleted: Joi.boolean().required()
+    },
+    params: {
+      id: Joi.string().hex()
+    }
+  }
+};
