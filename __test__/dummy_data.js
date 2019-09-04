ebbdf162021f0603bbf003d21bb9de718638dcd9
iKnowJavaScript <remymartins0398@yahoo.com>
2019-09-04 04:12:09 +0100

    Add a dummy data for testing

---
+++ __test__/dummy_data.js
@@ -0,0 +1,20 @@
+exports.users = {
+  user1: {
+    name: 'Victor',
+    email: 'remymartins@yahoo.com',
+    password: '22222222',
+    favoriteTeam: 'Arsenal'
+  },
+  user2: {
+    name: 'Chika Iwobi',
+    email: 'chika@yahoo.com',
+    password: '22222222',
+    favoriteTeam: 'Chelsea'
+  },
+  fakeUser: {
+    name: 'Dauda Seyi',
+    email: 'cyahoo.com',
+    password: '22222222',
+    favoriteTeam: 'Chelsea'
+  }
+};
